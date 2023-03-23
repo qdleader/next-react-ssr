af8051d2cbd4ae80c9f923a85320c4f02adc06d0
yangkun <yangkun@icourt.cc>
2023-03-23 15:00:53 +0800

    feat: init project

---
+++ pages/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const Home = () => (
+  <>
+    <div>我是首页</div>
+    <div><Link href="/pageA"><a>去A页面</a></Link></div>
+    <div><Link href="/pageB"><a>去B页面</a></Link></div>
+
+  </>
+)
+
+export default Home
+
