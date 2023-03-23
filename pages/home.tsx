8c083e90a41bdbbb8729c5bf7bb6eb71c62ca53c
yangkun <yangkun@icourt.cc>
2023-03-23 15:04:04 +0800

    feat: init project

--- pages/home.tsx
+++ pages/home.tsx
@@ -6,7 +6,7 @@ const Home = () => (
   <>
     <div>我是首页</div>
     <div><Link href="/pageA"><a>去A页面</a></Link></div>
-    <div><Link href="/pageB"><a>去B页面</a></Link></div>
+    <div><Link href="/pageB"><a>去B页面 </a></Link></div>
 
   </>
 )
