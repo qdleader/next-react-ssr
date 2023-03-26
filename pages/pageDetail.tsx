83bc9082bea9ab0de6eb7999e3f45a2329b3c1fc
前端架构师 <62106726+qdleader@users.noreply.github.com>
2023-03-26 18:17:00 +0800

    feat: 接口请求

--- pages/pageDetail.tsx
+++ pages/pageDetail.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
 import Link from 'next/link'
 import Footer from '@/components/footer'
+import { withRouter } from 'next/router'
+import { WithRouterProps } from 'next/dist/client/with-router'
 
-const pageDetail = () => (
-  <>
-    <div>我是详情页</div>
-    <div><Link href="/home">返回首页</Link></div>
-    <Footer></Footer>
-  </>
-)
+import axios from 'axios'
 
-export default pageDetail
+interface iData {
+  name: string,
+  age: 18
+}
+interface iProps extends WithRouterProps{
+  name: string
+  list:iData[]
+}
+const pageDetail = (props:iProps) => {
+  const { router ,list} = props
+  const name = router.query.name
+  
+  return (
+    <>
+      <div>我是{name}详情页</div>
+      
+      {
+        list?.map((item) => (
+          <div	key={item.name}>
+             <div>{item.name}</div>
+             <div>{item.age}</div>
+          </div>
+        ))}
+      <div><Link href="/home">返回首页</Link></div>
+      <Footer></Footer>
+    </>
+  )
+} 
+
+pageDetail.getInitialProps = async () => {
+
+  let res = await axios('https://admin-api-cy1w.onrender.com/list')
+  console.log("获取结果", res.data.data);
+  return { list : res.data.data };
+  // const promise = new Promise((resolve) => {
+  //   axios('https://admin-api-cy1w.onrender.com/list').then((res: { data: { data: unknown } }) => {
+  //     console.log("获取结果", res.data.data);
+  //     resolve(res.data)
+  //   })
+  // })
+  // return await promise
+}
+
+export default withRouter(pageDetail)
 
